4ae08d65d2f7652dcd97c149f8aa97351e2c47ab
Mohan Shil <mohanshil@Mohans-MacBook-Air.local>
2024-05-14 22:34:01 +0200

    init

---
+++ src/components/accordian/data.js
@@ -0,0 +1,26 @@
+
+
+ const data = [
+
+    {
+        id : '1',
+        question : "what is accordian ?",
+        answer: " Accordian components are user interface elements used for organizing and presenting content in a collapsing manner. They typically consist of a header, content and an expand/collapse action. ",
+    },
+    {
+        id : '2',
+        question : "what is accordian ?",
+        answer: " Accordian components are user interface elements used for organizing and presenting content in a collapsing manner. They typically consist of a header, content and an expand/collapse action. ",
+    },
+    {
+        id : '3',
+        question : "what is accordian ?",
+        answer:  "Accordian components are user interface elements used for organizing and presenting content in a collapsing manner. They typically consist of a header, content and an expand/collapse action."
+    },
+    {
+        id : '4',
+        question : "what is accordian ?",
+        answer: " Accordian components are user interface elements used for organizing and presenting content in a collapsing manner. They typically consist of a header, content and an expand/collapse action."
+    },
+];  
+export  default data;
